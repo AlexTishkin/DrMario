8bb50030b977589ed3d93ca69f1e7e616db8cda6
AlexTishkin <AlexTishkin57@yandex.ru>
2021-04-16 23:18:53 +0300

    Генерация изображений вирусов перенесена в отдельную фабрику (Рефактор)

--- DrMario/Display/Display.cs
+++ DrMario/Display/Display.cs
@@ -1,9 +1,7 @@
 ﻿using DrMario.Extensions;
+using DrMario.Factory;
 using DrMario.Interfaces;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace DrMario.Display
@@ -16,26 +14,18 @@ namespace DrMario.Display
         private readonly int _nextBlockRow = 1;
         private readonly int _nextBlockCol = 10;
 
-        private readonly Image _yellowVirus;
-        private readonly Image _blueVirus;
-        private readonly Image _redVirus;
+        private readonly VirusImageFactory _virusImageFactory;
 
         /// <summary>
         /// Высота и ширина ячейки (квадрат)
         /// </summary>
-        //private readonly int _cellSize = 29;
         private readonly int _cellSize = 36;
 
         public Display(IGame game, PictureBox window)
         {
             _game = game;
             _gameGrid = new DisplayGrid(window, _cellSize, _game.Field);
-
-            var imagesFolder = @"C:\Users\mi\Desktop\DrMario\DrMario\Images\";
-
-            _yellowVirus = ResizeImage(Image.FromFile(imagesFolder + "virus_yellow.png"), _cellSize, _cellSize);
-            _blueVirus = ResizeImage(Image.FromFile(imagesFolder + "virus_blue.png"), _cellSize, _cellSize);
-            _redVirus = ResizeImage(Image.FromFile(imagesFolder + "virus_red.png"), _cellSize, _cellSize);
+            _virusImageFactory = new VirusImageFactory(_cellSize);
         }
 
         public void ShowGameState()
@@ -58,19 +48,13 @@ namespace DrMario.Display
                     ((PictureBox)_gameGrid[row, col]).Image = null;
 
                     if (gameField[row, col].Type != GameCellType.Virus)
+                    {
                         _gameGrid[row, col].SetColor(gameField[row, col].Color);
+                    }
 
                     if (gameField[row, col].Type == GameCellType.Virus)
                     {
-                        // TODO: To Factory...
-                        Image virusImage = null;
-                        if (gameField[row, col].Color == GameCellColor.Yellow)
-                            virusImage = _yellowVirus;
-                        else if (gameField[row, col].Color == GameCellColor.Blue)
-                            virusImage = _blueVirus;
-                        else if (gameField[row, col].Color == GameCellColor.Red)
-                            virusImage = _redVirus;
-
+                        var virusImage = _virusImageFactory.CreateVirusImage(gameField[row, col].Color);
                         ((PictureBox)_gameGrid[row, col]).Image = virusImage;
                     }
                 }
@@ -102,42 +86,5 @@ namespace DrMario.Display
             foreach (var cell in cells)
                 _gameGrid[cell.Row, cell.Column].SetColor(cell.Color);
         }
-
-        // Utility
-
-        /// <summary>
-        /// Resize the image to the specified width and height.
-        /// </summary>
-        /// <param name="image">The image to resize.</param>
-        /// <param name="width">The width to resize to.</param>
-        /// <param name="height">The height to resize to.</param>
-        /// <returns>The resized image.</returns>
-        public static Bitmap ResizeImage(Image image, int width, int height)
-        {
-            var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
-
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                using (var wrapMode = new ImageAttributes())
-                {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-
-            return destImage;
-        }
-
-        //
-
     }
 }
---
+++ DrMario/Factory/VirusImageFactory.cs
@@ -0,0 +1,33 @@
+﻿using DrMario.Extensions;
+using System;
+using System.Drawing;
+
+namespace DrMario.Factory
+{
+    public class VirusImageFactory
+    {
+        private readonly string _imagesFolderPath;
+        private readonly Image _yellowVirus;
+        private readonly Image _blueVirus;
+        private readonly Image _redVirus;
+
+        private readonly int _cellSize;
+
+        public VirusImageFactory(int cellSize)
+        {
+            _cellSize = cellSize;
+            _imagesFolderPath = @"C:\Users\mi\Desktop\DrMario\DrMario\Images\";
+            _yellowVirus = Image.FromFile(_imagesFolderPath + "virus_yellow.png").ResizeImage(_cellSize, _cellSize);
+            _blueVirus = Image.FromFile(_imagesFolderPath + "virus_blue.png").ResizeImage(_cellSize, _cellSize);
+            _redVirus = Image.FromFile(_imagesFolderPath + "virus_red.png").ResizeImage(_cellSize, _cellSize);
+        }
+
+        public Image CreateVirusImage(GameCellColor color)
+        {
+            if (color == GameCellColor.Yellow) return _yellowVirus;
+            if (color == GameCellColor.Blue) return _blueVirus;
+            if (color == GameCellColor.Red) return _redVirus;
+            throw new ArgumentException("Incorrect color (GameCellColor)");
+        }
+    }
+}
